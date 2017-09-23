d8caaf5d1eb31fcaee033e1c523b8bf10a416963
三硝基甲苯 <318388589@qq.com>
2017-09-23 16:56:58 +0800

    Upload tetris.h tetris.c Makefile conio.h

---
+++ tetris.h
@@ -0,0 +1,10 @@
+#ifndef __TETRIS_H__
+#define __TETRIS_H__
+
+        /* the board */
+#define      B_COLS 12
+#define      B_ROWS 23
+#define      B_SIZE (B_ROWS * B_COLS)
+
+#endif  /* __TETRIS_H__ */
+
